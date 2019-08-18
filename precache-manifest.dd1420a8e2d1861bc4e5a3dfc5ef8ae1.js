ed7e64e06403db2ac7788dd318e9d891318ebaf5
mriedman <33470702+mriedman@users.noreply.github.com>
2019-08-18 15:46:30 -0400

    Add files via upload

---
+++ precache-manifest.dd1420a8e2d1861bc4e5a3dfc5ef8ae1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06352792a05e755489779d62c26effe6",
+    "url": "mriedman.github.io/bracelet/index.html"
+  },
+  {
+    "revision": "3b0e289657e87985e154",
+    "url": "mriedman.github.io/bracelet/static/css/main.7bd0faa2.chunk.css"
+  },
+  {
+    "revision": "ff491cbb73b92a330d18",
+    "url": "mriedman.github.io/bracelet/static/js/2.8b6ec6c3.chunk.js"
+  },
+  {
+    "revision": "3b0e289657e87985e154",
+    "url": "mriedman.github.io/bracelet/static/js/main.acf97968.chunk.js"
+  },
+  {
+    "revision": "2db768729e44551c91ed",
+    "url": "mriedman.github.io/bracelet/static/js/runtime~main.2e836d80.js"
+  }
+]);
